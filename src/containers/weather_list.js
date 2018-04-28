b55123eaf367b799ead656df2522ee681e5a94d2
yotsaphonSutweha <komsan_58@hotmail.com>
2018-04-29 00:05:06 +0100

    set up weather list container and link to the action

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component{
+    renderWeather(cityData) {
+        const cityName = cityData.city.name;
+        return (
+            <tr key={cityName}>
+                <td>{cityName}</td>
+            </tr>
+        );
+    }
+
     render(){
         return(
             <table className="table table-hover">
@@ -14,7 +23,7 @@ class WeatherList extends Component{
                     </tr>            
                 </thead>
                 <tbody>
-
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
